e4c546036a0963918bd0a23d86c44b549334a3f2
unknown <sirving@.mobileepiphany.local>
2011-05-16 09:17:02 -0600

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MSMarket::Application.config.secret_token = 'aef70048d59b79241d069fd7eabdec354f7f5c75c26a056df0bf1af72de4be3b6a81fd99747383756b87091fbbc6e12f0401f9832e2e20dd8df4663f5f26bde3'
