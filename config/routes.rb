00a3af3b97379a3e78859ccf039be22c343ca8ef
unknown <sirving@.mobileepiphany.local>
2011-06-02 09:45:02 -0600

    Changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,52 @@
 MSMarket::Application.routes.draw do
+
+  get "worlds/index"
+
+  get "worlds/show"
+
+  get "worlds/edit"
+
+  get "worlds/create"
+
+  get "characters/new"
+
+  get "characters/edit"
+
+  get "characters/destroy"
+
+   resources :users do
+    member do
+
+    end
+  end
+  resources :sessions, :only => [:new, :create, :destroy]
+  resources :characters
+  resources :worlds, :only => [:index, :create, :destroy]
+
+  root :to => 'pages#home'
+  get "pages/home"
+  get "pages/about"
+  get "pages/contact"
+
+  get "characters/my_characters"
+
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+
+  match "/signin",  :to => 'sessions#new'
+  match "/signout", :to => 'sessions#destroy'
+
+  # user routes
+  match "/signup", :to => 'users#new'
+  match "/my_characters", :to => 'users#my_characters'
+
+  match "/createcharacter", :to => 'characters#new'
+
+  match "/worlds", :to => "worlds#index"
+
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
